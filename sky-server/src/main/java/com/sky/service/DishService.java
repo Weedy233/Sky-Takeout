5ef583af538c4ed3aa4b933006f7f2a49d472417
weedy233 <hy_tu_zhi_233@outlook.com>
2025-10-27 17:24:35 +0800

    feat: Implement dish add

---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,14 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+
+public interface DishService {
+    
+    /**
+     * 新增菜品和对应的口味
+     * @param dishDTO 菜品 DTO
+     */
+    Dish saveWithFlavor(DishDTO dishDTO);
+    
+}
