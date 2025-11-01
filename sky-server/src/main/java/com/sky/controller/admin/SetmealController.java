51ee6aa15d45208ccd669e35aba125da6864196a
weedy233 <hy_tu_zhi_233@outlook.com>
2025-11-01 10:45:24 +0800

    feat: Add SetmealController

---
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -0,0 +1,122 @@
+package com.sky.controller.admin;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.entity.Setmeal;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.SetmealService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+/**
+ * 套餐管理
+ */
+@RestController
+@RequestMapping("/admin/setmeal")
+@Slf4j
+@Api(tags = "员工管理相关接口")
+public class SetmealController {
+
+    @Autowired
+    private SetmealService setmealService;
+    
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增套餐")
+    public Result save(@RequestBody SetmealDTO setmealDTO) {
+        log.info("新增套餐：{}", setmealDTO);
+        setmealService.save(setmealDTO);
+        return Result.success();
+    }
+
+    /**
+     * 根据id查询套餐 
+     * @param id
+     * @return 
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询套餐")
+    public Result<Setmeal> getById(@PathVariable Long id) {
+        log.info("获取套餐：{}", id);
+        Setmeal setmeal = setmealService.getById(id);
+        return Result.success(setmeal);
+    }
+
+    /**
+     * 更改套餐信息
+     * @param setmealDTO
+     * @return
+     */
+    @PutMapping()
+    @ApiOperation("更改套餐信息")
+    public Result update(@RequestBody SetmealDTO setmealDTO) {
+        log.info("更新套餐信息：{}", setmealDTO);
+        setmealService.update(setmealDTO);
+        return Result.success();
+    }
+
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("套餐分页查询")
+    public Result<PageResult> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+        log.info("分页查询套餐：{}", setmealPageQueryDTO);
+        PageResult result = setmealService.pageQuery(setmealPageQueryDTO);
+        return Result.success(result);
+    }
+    
+    
+    /**
+     * 套餐起售停售
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("套餐起售停售")
+    public Result enableOrDisable(@PathVariable Integer status, Long id) {
+        log.info("起售禁售套餐：{}{}", status, id);
+        setmealService.enableOrDisable(status, id);
+        return Result.success();
+    }
+
+
+    /**
+     * 批量删除菜品
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    @ApiOperation("批量删除套餐")
+    public Result delete(@RequestParam List<Long> ids) {
+        log.info("批量删除菜品：{}", ids);
+        setmealService.deleteBatch(ids);
+        return Result.success();
+    }
+}
